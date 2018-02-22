3976c4bcf29d1df7961af504ce2f94d1affa6af5
4rkiel <4rkiel@gmail.com>
2018-02-22 03:25:45 +0100

    option

--- intro.cpp
+++ intro.cpp
@@ -4,86 +4,126 @@ Intro::Intro () {
 
     layout = new QGridLayout;
     layout -> setAlignment(Qt::AlignCenter);
-
-    introBox = new QWidget;
-    box = new QVBoxLayout;
-
-    effect = new QGraphicsDropShadowEffect(this);
-    effect -> setBlurRadius(5);
-    effect -> setXOffset(0);
-    effect -> setYOffset(5);
-    effect -> setColor(QColor(0,0,0,150));
-
-    introBox -> setGraphicsEffect(effect);
+    layout -> setSpacing(0);
+    layout -> setMargin(0);
 
 
+        // Intro Menu 
 
-    // New Game Button
-  
-    QString strButton = QString::fromUtf8("Duel");
-    newgame = new ShadowButt("\uf439", strButton);
+        introBox = new QWidget;
+        introBox -> setObjectName("introBox");
+        box = new QVBoxLayout;
+
+        effect = new QGraphicsDropShadowEffect(this);
+        effect -> setBlurRadius(5);
+        effect -> setXOffset(0);
+        effect -> setYOffset(5);
+        effect -> setColor(QColor(0,0,0,150));
+
+        introBox -> setGraphicsEffect(effect);
+
+
+            // New Game Button
+          
+            QString strButton = QString::fromUtf8("Duel");
+            newgame = new ShadowButt("\uf439", strButton);
+            newgame -> setToolTip("Commencer une partie");
+            connect(newgame, SIGNAL(clicked()), this, SLOT(emitNewStack()));
+            box -> addWidget(newgame);
+
+
+            // Options Button
+
+            QString strDeck = QString::fromUtf8("Decks");
+            decks = new ShadowButt("\uf02d", strDeck);
+            decks -> setToolTip("Gestion des decks");
+            box -> addWidget(decks);
+
+
+            // Regles Button
+
+            QString strRules = QString::fromUtf8("Règles");
+            rules = new ShadowButt("\uf24e", strRules);
+            rules -> setToolTip("Gestion de l'arbitrage");
+            box -> addWidget(rules);
+
+
+            // Options Button
+
+            QString strOptions = QString::fromUtf8("Options");
+            options = new ShadowButt("\uf085", strOptions);
+            options -> setToolTip("Gestion des options");
+            connect(options, SIGNAL(clicked()), this, SLOT(showOptions()));
+            box -> addWidget(options);
+
+
+            // Quit Button
+
+            QString strQuit = QString::fromUtf8("Quitter");
+            quit = new ShadowButt("\uf011", strQuit);
+            quit -> setToolTip("Quitter l'application");
+            connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+            box -> addWidget(quit);
+
+
+        introBox -> setLayout(box);
+        layout -> addWidget(introBox, 0, 0, 1, 1);
+
+
+
+        // Shadow
+        
+        shadow = new QPushButton;
+        shadow -> setObjectName("shadow");
+        shadow -> setVisible(false);
+        shadow -> setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        connect(shadow, SIGNAL(clicked()), this, SLOT(showIntro()));
+        
+        layout -> addWidget(shadow, 0, 0, 1, 1);
+        
+
+        // Options
+        
+        optionBox = new QWidget;
+        optionBox -> setObjectName("optionBox");
+        optionBox -> setVisible(false);
+
+        optionLayout = new QVBoxLayout;
+        optionLayout -> setSpacing(0);
+        optionLayout -> setMargin(0);
+        optionLayout -> setAlignment(Qt::AlignCenter);
+
+            tabBox = new QTabWidget;
+
+                optionTab = new QWidget;
+                optionTab -> setObjectName("optionTab");
+                optionTabLayout = new QVBoxLayout;
+
+                    optLabel = new QLabel();
+                    optLabel -> setText("yoooooo");
+
+                    optionTabLayout -> addWidget(optLabel);
+
+                optionTab -> setLayout(optionTabLayout);
+                tabBox -> addTab(optionTab, "Options");
+
     
-    newgame -> setToolTip("Commencer une partie");
+                aboutTab = new QWidget;
+                aboutTab -> setObjectName("aboutTab");
+                aboutTabLayout = new QVBoxLayout;
  
-    connect(newgame, SIGNAL(clicked()), this, SLOT(emitNewStack()));
-    
-    box -> addWidget(newgame);
+                    abtLabel = new QLabel();
+                    abtLabel -> setText("wuuuu");
 
+                    aboutTabLayout -> addWidget(abtLabel);
 
+                aboutTab -> setLayout(aboutTabLayout);
+                tabBox -> addTab(aboutTab, "A Propos");
 
-    
-    // Options Button
+            optionLayout -> addWidget(tabBox, 0, 0);
 
-    QString strDeck = QString::fromUtf8("Decks");
-    decks = new ShadowButt("\uf02d", strDeck);
-    
-    decks -> setToolTip("Gestion des decks");
-
-    box -> addWidget(decks);
-
-
-
-
-    // Regles Button
-
-    QString strRules = QString::fromUtf8("Règles");
-    rules = new ShadowButt("\uf24e", strRules);
-     
-    rules -> setToolTip("Gestion de l'arbitrage");
-
-    box -> addWidget(rules);
-
-
-
-    // Options Button
-
-    QString strOptions = QString::fromUtf8("Options");
-    options = new ShadowButt("\uf085", strOptions);
-     
-    options -> setToolTip("Gestion des options");
-
-    box -> addWidget(options);
-
-
-
-
-    // Quit Button
-
-    QString strQuit = QString::fromUtf8("Quitter");
-    quit = new ShadowButt("\uf011", strQuit);
-     
-    quit -> setToolTip("Quitter l'application");
-
-    connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
-    
-    box -> addWidget(quit);
-
-
-
-
-    introBox -> setLayout(box);
-
-    layout -> addWidget(introBox, 0, 0);
+        optionBox -> setLayout(optionLayout);
+        layout -> addWidget(optionBox, 0, 0, 1, 1, Qt::AlignCenter);
 
     this -> setLayout(layout);
 }
@@ -102,6 +142,18 @@ Intro::~Intro (){
     delete box;
     delete introBox;
     delete layout;
+
+    delete shadow;
+
+    delete optLabel;
+    delete optionTabLayout;
+    delete optionTab;
+    delete abtLabel;
+    delete aboutTabLayout;
+    delete aboutTab;
+    delete tabBox;
+    delete optionLayout;
+    delete optionBox;
 }
 
 
@@ -109,7 +161,27 @@ void Intro::init (){
     newgame -> setFocus();
 }
 
+
 void Intro::emitNewStack (){
         emit newStack();
 }
 
+
+void Intro::showOptions (){
+    introBox -> setVisible(false);
+    shadow -> setVisible(true);
+    optionBox -> setVisible(true);
+}
+
+
+void Intro::showRules (){
+    introBox -> setVisible(false);
+    shadow -> setVisible(true);
+}
+
+
+void Intro::showIntro (){
+    optionBox -> setVisible(false);
+    shadow -> setVisible(false);
+    introBox -> setVisible(true);    
+}
--- intro.h
+++ intro.h
@@ -12,6 +12,9 @@
 #include <QPushButton>
 #include <QString>
 
+#include <QTabWidget>
+
+
 #include "shadowButt.h"
 
 
@@ -26,6 +29,9 @@ class Intro : public QFrame {
 
     public slots:
     void emitNewStack();
+    void showOptions();
+    void showRules();
+    void showIntro();
 
     signals:
     void newStack();
@@ -33,16 +39,27 @@ class Intro : public QFrame {
     private:
     QGridLayout * layout;
     QWidget * introBox;
-    QVBoxLayout * box;
+    
+        QVBoxLayout * box;
+        QGraphicsDropShadowEffect * effect;
 
-    QGraphicsDropShadowEffect * effect;
+        ShadowButt * newgame;
+        ShadowButt * decks;
+        ShadowButt * rules;
+        ShadowButt * options;
+        ShadowButt * quit;
 
-    ShadowButt * newgame;
-    ShadowButt * decks;
-    ShadowButt * rules;
-    ShadowButt * options;
-    ShadowButt * quit;
- 
+    QPushButton * shadow;
+
+    QWidget * optionBox;
+    QVBoxLayout * optionLayout;
+        QTabWidget * tabBox;
+        QWidget * optionTab;
+        QVBoxLayout * optionTabLayout;
+        QLabel * optLabel;
+        QWidget * aboutTab;
+        QVBoxLayout * aboutTabLayout;
+        QLabel * abtLabel;
 };
 
 
