f82e5207f5c0c52baf047bb6c9fb3459d7114c43
4rk <4rkiel@gmail.com>
2018-03-10 22:44:46 +0100

    Card Template

---
+++ inc/helptab.h
@@ -0,0 +1,88 @@
+#ifndef RUTAB
+#define RUTAB
+
+#include <QWidget>
+#include <QFrame>
+
+#include <QSettings>
+
+#include <QStyle>
+#include <QGraphicsDropShadowEffect>
+
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QStackedLayout>
+
+#include <QScrollArea>
+#include <QPushButton>
+#include <QLabel>
+#include <QFile>
+#include <QTextStream>
+#include <QCheckBox>
+#include <QSpacerItem>
+#include <QComboBox>
+
+#include "../inc/shadowButt.h"
+
+class RuleTab : public QFrame {
+
+    Q_OBJECT
+    
+    public:
+    RuleTab();
+    ~RuleTab();
+    void init();
+    void reInit();
+
+    private:
+    QGraphicsDropShadowEffect * ruEffect;
+    QGridLayout * layout;
+
+    QWidget * tabBox;
+    QGridLayout * tabLayout;
+
+    QPushButton * currButt;
+
+    QPushButton * persoButt;
+    QPushButton * rulesButt;
+    ShadowButt * exitButt;
+
+    QWidget * ruleBox;
+    QStackedLayout * ruleLayout;
+
+    QScrollArea * persoScroll;
+    QWidget * persoScrollBox;
+    QVBoxLayout * persoPaneLayout;
+/*
+        QCheckBox * shareChck;
+        QLabel * shareDesc;
+
+        QComboBox * langInput;
+        QLabel * langDesc;
+
+        QLabel * shortcutDesc;
+*/
+
+    QScrollArea * ruleScroll;
+    QLabel * ruleLabel;
+
+    void updateStyle(QPushButton * b);
+    void loadPersoSettings();
+
+    public slots:
+    void setPerso();
+    void setRules();
+    void emitClose();
+/*
+    void shareChange();   
+    void contrasteChange();
+    void achromaChange();
+    void largeChange();
+*/
+    signals:
+    void introStack();
+};
+
+
+#endif
---
+++ src/helptab.cpp
@@ -0,0 +1,296 @@
+#include "../inc/ruletab.h"
+
+RuleTab::RuleTab (){
+
+    layout = new QGridLayout;
+    layout -> setSpacing(0);
+    layout -> setMargin(0);
+    
+    ruEffect = new QGraphicsDropShadowEffect(this);
+    ruEffect -> setBlurRadius(5);
+    ruEffect -> setXOffset(0);
+    ruEffect -> setYOffset(5);
+    ruEffect -> setColor(QColor(0,0,0,150));
+
+    setGraphicsEffect(ruEffect);
+
+        // Tab box ............................................................
+
+        tabBox = new QWidget;
+        tabBox -> setObjectName("ruleTab");
+        tabLayout = new QGridLayout;
+        tabLayout -> setSpacing(0);
+        tabLayout -> setMargin(0);
+  
+            exitButt = new ShadowButt("\uf00d", "");
+            exitButt -> setToolTip("Fermer les règles");
+            connect(exitButt, SIGNAL(clicked()), this, SLOT(emitClose()));
+            
+            tabLayout -> addWidget(exitButt, 0, 4, 1, 1);
+
+
+            persoButt = new QPushButton;
+            persoButt -> setText("Personnalisation");
+            persoButt -> setProperty("down", true);
+            connect(persoButt, SIGNAL(clicked()), this, SLOT(setPerso()));
+
+            tabLayout -> addWidget(persoButt, 0, 0, 1, 1);
+            
+
+            rulesButt = new QPushButton;
+            rulesButt -> setText("Règles du Jeu");
+            rulesButt -> setProperty("down", false);
+            connect(rulesButt, SIGNAL(clicked()), this, SLOT(setRules()));
+            
+            tabLayout -> addWidget(rulesButt, 0, 1, 1, 1);
+
+
+            QSpacerItem * spacerButt = new QSpacerItem(1,1,
+                    QSizePolicy::Expanding,QSizePolicy::Preferred);
+            
+            tabLayout -> addItem(spacerButt, 0, 2);
+
+        tabBox -> setLayout(tabLayout);
+
+
+    layout -> addWidget(tabBox, 0, 0, 1, 1);
+
+
+
+        // Label Box ..........................................................
+
+        ruleBox = new QWidget;
+        ruleBox -> setObjectName("ruleBox");
+        
+        ruleLayout = new QStackedLayout;
+        ruleLayout -> setSpacing(0);
+        ruleLayout -> setMargin(0);
+
+
+            // Personnalisation ...............................................
+
+            persoScroll = new QScrollArea;
+            persoScroll -> setFrameShape(QFrame::NoFrame);
+            persoScroll -> setWidgetResizable(true);
+            persoScroll -> setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+            persoScroll -> setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+            persoScroll -> setFocusPolicy(Qt::NoFocus);
+
+            persoScrollBox = new QWidget;
+            persoPaneLayout = new QVBoxLayout;
+            persoPaneLayout -> setAlignment(Qt::AlignTop);
+
+            /*
+                // Share
+                
+                shareChck = new QCheckBox;
+                shareChck -> setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+                shareChck -> setText("Partage");
+                connect(shareChck, SIGNAL(toggled(bool)), this, SLOT(shareChange())); 
+                persoPaneLayout -> addWidget(shareChck);
+
+                shareDesc = new QLabel;
+                shareDesc -> setWordWrap(tperse);
+                shareDesc -> setTextInteractionFlags(Qt::NoTextInteraction);
+                shareDesc -> setText("Partage les données d'utilisation");
+                persoPaneLayout -> addWidget(shareDesc);
+
+
+                // Langage
+                
+                langInput = new QComboBox;
+                langInput -> setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+                langInput -> addItem("Français");
+                langInput -> addItem("English");
+                persoPaneLayout -> addWidget(langInput);
+
+                langDesc = new QLabel;
+                langDesc -> setTextInteractionFlags(Qt::NoTextInteraction);
+                langDesc -> setText("Langue de l'interface");
+                persoPaneLayout -> addWidget(langDesc);
+
+                // Shortcut
+
+                shortcutDesc = new QLabel;
+                shortcutDesc -> setTextInteractionFlags(Qt::NoTextInteraction);
+                shortcutDesc -> setText("Raccourcis Clavier");
+                persoPaneLayout -> addWidget(shortcutDesc);
+            
+            */
+//
+                // load settings
+
+                loadPersoSettings();
+
+
+            persoScrollBox -> setLayout(persoPaneLayout); 
+            persoScroll -> setWidget(persoScrollBox);
+
+            ruleLayout -> addWidget(persoScroll);
+
+           
+
+
+            // About ..........................................................
+
+            ruleScroll = new QScrollArea;
+            ruleScroll -> setFrameShape(QFrame::NoFrame);
+            ruleScroll -> setWidgetResizable(true);
+            ruleScroll -> setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+            ruleScroll -> setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+ 
+
+                ruleLabel = new QLabel;
+
+                    QFile file("i18n/fr_FR/rules.text");
+
+                    QString text = "";
+                    QString line;
+                    if (file.open(QIODevice::ReadOnly | QIODevice::Text)){
+                        QTextStream stream(&file);
+                        while (!stream.atEnd()){
+                            line = stream.readLine();
+                            text = text + line + "\n";
+                        }
+                    }
+                    file.close();
+
+                ruleLabel -> setText(text);
+                ruleLabel -> setWordWrap(true);
+                ruleLabel -> setTextInteractionFlags(Qt::TextSelectableByMouse);
+                ruleLabel->setTextFormat(Qt::RichText);
+                ruleLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+                ruleLabel->setOpenExternalLinks(true);
+
+            ruleScroll -> setWidget(ruleLabel);
+            
+            ruleLayout -> addWidget(ruleScroll);
+
+
+        ruleLayout -> setCurrentWidget(persoScroll);
+        ruleBox -> setLayout(ruleLayout);
+
+    layout -> addWidget(ruleBox, 1, 0, 1, 2);
+
+    setLayout(layout);
+
+}
+
+
+RuleTab::~RuleTab (){
+
+    delete ruEffect;
+
+    delete ruleLabel;
+    delete ruleScroll;
+
+    delete persoPaneLayout;
+    delete persoScrollBox;
+    delete persoScroll;
+
+        delete exitButt;
+        delete rulesButt;
+        delete persoButt;
+    
+    delete tabLayout;
+    delete tabBox;
+    delete ruleLayout;
+
+    delete ruleBox;
+
+    delete layout;
+}
+
+
+void RuleTab::init (){
+
+    currButt = persoButt;
+    persoButt -> setFocus();
+}
+
+void RuleTab::reInit (){
+
+    updateStyle(currButt);
+}
+
+
+void RuleTab::setRules (){
+
+    updateStyle(rulesButt);
+
+        ruleLayout -> setCurrentWidget(ruleScroll);
+    }
+
+
+void RuleTab::setPerso (){
+
+    updateStyle(persoButt);
+
+    ruleLayout -> setCurrentWidget(persoScroll);
+}
+
+
+void RuleTab::updateStyle (QPushButton * b){
+     
+    currButt -> setProperty("down", false);
+    b -> setProperty("down", true);
+
+    currButt -> style() -> unpolish(currButt);
+    currButt -> style() -> polish(currButt);
+    
+    b -> style() -> unpolish(b);
+    b -> style() -> polish(b);
+
+    currButt = b;
+}
+
+
+void RuleTab::emitClose (){
+
+    emit introStack();
+}
+
+
+void RuleTab::loadPersoSettings (){
+/*
+    QSettings settings;
+    
+    if (settings.value("share", Qt::Unchecked).toBool()){
+        shareChck -> setCheckState(Qt::Checked);
+    }
+*/
+}
+
+/*
+void RuleTab::shareChange (){
+
+    QSettings settings;
+    settings.setValue("share", shareChck -> isChecked());
+}
+
+
+
+void RuleTab::contrasteChange (){
+
+    QSettings settings;
+    settings.setValue("contraste", contrasteChck -> isChecked());
+}
+
+
+
+void RuleTab::achromaChange (){
+
+    QSettings settings;
+    settings.setValue("achroma", achromaChck -> isChecked());
+}
+
+
+
+void RuleTab::largeChange (){
+
+    QSettings settings;
+    settings.setValue("large", largeChck -> isChecked());
+}
+
+*/
+
