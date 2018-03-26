a7903e62c6fdbed973c3a036f0a6f05360e6e71c
MEYER Adrien <Wargamme@gmail.com>
2018-03-26 12:54:00 +0200

    starting deck creator

--- inc/deckedit.h
+++ inc/deckedit.h
@@ -2,13 +2,51 @@
 #define DECKEDIT_H
 
 #include <QWidget>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QComboBox>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QCompleter>
+#include <QFileDialog>
+#include <QCoreApplication>
+#include <QStringList>
+#include <QDebug>
+#include <algorithm>
+
+enum buttonDeckEdit
+{
+    QUITTER,
+    MELANGER,
+    TRIER,
+    EFFACER,
+    SAUVER,
+    CREER,
+    SUPPRIMER,
+    NBR_BUTTON_DECK_EDIT
+};
 
 class deckEdit : public QWidget
 {
     Q_OBJECT
 
     public:
-        explicit deckEdit(QWidget *parent = nullptr);
+        explicit deckEdit();
+
+        const QString appPath = QCoreApplication::applicationDirPath();
+        const QString imgRep = appPath+"/img/img_attr/";
+        const QString deckRep = appPath+"/deck/";
+
+    private:
+        void createEdit();
+
+        QComboBox *selectDeck;
+        QLineEdit *newDeck;
+        QPushButton *tabBut[NBR_BUTTON_DECK_EDIT];
 
     signals:
 
--- src/deckedit.cpp
+++ src/deckedit.cpp
@@ -1,6 +1,49 @@
 #include "../inc/deckedit.h"
 
-deckEdit::deckEdit(QWidget *parent)
-{
 
+deckEdit::deckEdit()
+{
+    selectDeck = new QComboBox;
+    newDeck = new QLineEdit;
+
+    for(int i=0; i<NBR_BUTTON_DECK_EDIT; i++)
+    {
+        tabBut[i] = new QPushButton;
+    }
+
+    QGridLayout *mainLayout = new QGridLayout;
+    //{
+        // ... selecteur / creation de Deck ....................................
+
+        QFrame *editCreate = new QFrame;
+        editCreate->setStyleSheet("background-color: #F44336);");
+        QGridLayout *editCreateLayout = new QGridLayout;
+        editCreate->setLayout(editCreateLayout);
+        //{
+            // ... selecteur de Deck ...........................................
+            QStringList deckList = QDir(deckRep).entryList({"*.deck"});
+
+            /* on tronc l'extension ".deck" avant de placer dans la QComboBox */
+            foreach (QString str, deckList)
+            {
+                str.chop(5);
+                selectDeck->addItem(str);
+            }
+
+            QFormLayout *formulaire = new QFormLayout;
+            formulaire->addRow("Deck: ", selectDeck);
+
+            editCreateLayout->addLayout(formulaire, 0, 0);
+
+            // ... name a deck .................................................
+            tabBut[QUITTER]->setText(tr("Quitter"));
+            formulaire->addRow(tabBut[QUITTER], newDeck);
+
+            //TODO signal qui crée le deck
+        //}
+
+        mainLayout->addWidget(editCreate);
+
+    //}
+    setLayout(mainLayout);
 }
