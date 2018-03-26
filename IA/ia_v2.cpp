99403d05ec2b32e130b62fa15f070d6fd2c63db3
schott97L <lucas.schott97@gmail.com>
2018-03-26 15:26:27 +0200

    développemnt en cours de la fonction learn du réseau neuronal

--- IA/ia_v2.cpp
+++ IA/ia_v2.cpp
@@ -3,22 +3,32 @@
 #include <time.h>
 #include <eigen-git-mirror/Eigen/Core>
 #include <vector>
+#include <tuple>
 
 using namespace Eigen;
+using namespace std;
 
 class Network
 {
     public:
         
-        
+        /*neural netwrok
+         * 4 layers: 1 input layer, 2 hidden layers, 1 output layer
+         * synaps are the weights between the neurons layer
+         */
         Matrix<float,20,60> input_synaps;
         Matrix<float,60,60> hidden_synaps;
         Matrix<float,60,10> output_synaps;
         
+        /*memory of the neural network
+         * matrix associated with his previous computated Q_value
+         */
+        vector<tuple<Matrix<float,1,20>,float>> memory;
         
+        
+        //neural network weights initialisation
         void init()
         {
-            
             int i,j;
             srand (time(NULL));
             
@@ -46,6 +56,7 @@ class Network
         }
         
         
+        //compute the vector of actions q_values
         Matrix<float,1,10> compute_choices(Matrix<float,1,20> game_state)
         {
             Matrix<float,1,10> actions;
@@ -70,6 +81,10 @@ class Network
         }
         
         
+        /*choose the action to do, function of the actions' q_value calculated
+         * much an action has a big q_value, much more it has chance to be
+         * chosen
+         */
         int choose_action(Matrix<float,1,10> actions)
         {
             srand (time(NULL));
@@ -99,4 +114,44 @@ class Network
             
             return action;
         }
+        
+        
+        int play_learn(Matrix<float,1,20> game_state)
+        {
+            Matrix<float,1,20> target_states[20];
+            float q_targets[20];
+            Matrix<float,1,10> q_values = compute_choices(game_state);
+
+            int action;
+            for(action=0;action<20;action++)
+            {
+                //compute the next state if this action in chosen
+                target_states[action] = play_simulation(game_state, action);
+                //compute the q_value of this state, function of the memory
+                q_targets[action] = compute_q_value(target_states[action]);
+            }
+
+            int i;
+            float td_loss=0;
+            for(i=0;i<20;i++)
+                td_loss += pow(q_targets[i]-q_values(i),2);
+
+            //stochastic gradient descent, reinitialize at each iteration
+            //backward function, backpropagation
+            //update the weight
+            
+            int played_action = choose_action(q_values);
+            return played_action;
+        }
+        
+        
+        void upatde(last_reward, signal)
+        {
+            float new_state = tensor(signal);
+            memory.push(last_state,new_state,last_action,last_reward)
+        }
+
+
+
+
 };
