c0a5fec38d311cb3ae1c20d419b81b771906c976
MEYER Adrien <Wargamme@gmail.com>
2018-03-28 00:20:57 +0200

    bouton bouton bouton

--- inc/deckedit.h
+++ inc/deckedit.h
@@ -8,6 +8,7 @@
 #include <QPushButton>
 #include <QTextEdit>
 #include <QComboBox>
+#include <QSpinBox>
 #include <QGridLayout>
 #include <QVBoxLayout>
 #include <QFormLayout>
@@ -21,6 +22,7 @@
 
 #define NBR_CARTE_DECK_VISU 60 // multiple de 10
 #define NBR_CARTE_EXTRA_DECK 15
+
 class deckEdit : public QWidget
 {
     enum buttonDeckEdit
@@ -35,23 +37,45 @@ class deckEdit : public QWidget
         NBR_BUTTON_DECK_EDIT
     };
 
+    enum buttonFiltre
+    {
+        EFFACER_RECHERCHE,
+        FILTRER,
+        NBR_BUTTON_FILTRE
+    };
+
     Q_OBJECT
 
     public:
-        explicit deckEdit();
-
         const QString appPath = QCoreApplication::applicationDirPath();
         const QString imgRep = appPath+"/img/img_attr/";
+        const QString defaultImage = imgRep + "DEFAULT.jpg";
         const QString deckRep = appPath+"/deck/";
+
         const QStringList buttonName = {tr("Quitter"), tr("Mélanger"), tr("Trier"),
                                         tr("Effacer"), tr("Sauvegarder"), tr("Créer"),
                                         tr("Supprimer")};
+        const QStringList genreList = {"Monstre", tr("Magie"), tr("Piège")};
+        const QStringList sousGenreList = {tr("Effet"), tr("Normal"), tr("Fusion"), tr("Toon"), tr("Rituel")};
+
+        explicit deckEdit();
 
     private:
         void createEdit();
 
         QComboBox *selectDeck;
+        QComboBox *choixGenre;
+        QComboBox *choixSousGenre;
+        QComboBox *choixType;
+
         QLineEdit *newDeck;
+        QLineEdit *textSearch;
+
+        QSpinBox *spinNiveau;
+        QSpinBox *spinAtk;
+        QSpinBox *spinDef;
+
+
         QPushButton *tabBut[NBR_BUTTON_DECK_EDIT];
         QPushButton *tabCardVisu[NBR_CARTE_DECK_VISU];
         QPushButton *tabExtraDeck[NBR_CARTE_EXTRA_DECK];
