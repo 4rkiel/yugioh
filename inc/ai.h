cef81f36e699d987faa1bfd60e51726a6dc516bc
schott97l <lucas.schott97@gmail.com>
2018-04-04 15:39:05 +0200

    chargement et sauvegarde d'une IA dans un fichier : fonctionnel

--- inc/ai.h
+++ inc/ai.h
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
-#include <../IA/lib_eigen/Eigen/Core>
+#include "../IA/lib_eigen/Eigen/Core"
 #include <vector>
 
 using namespace Eigen;
@@ -13,23 +13,23 @@ using namespace std;
 
 class Ai {
 
-    private:
+    public:
         
         //matrix of weights
-        vector<Matrix<float,Dynamic,Dynamic>> hidden_weights;
-        Matrix<float,Dynamic,10> output_weight;
+        vector<Matrix<float,60,60>> hidden_weights;
+        Matrix<float,60,10> output_weight;
         
         //matrix of deltas, which are the difference between the current weight,
         //and the previous weight, at each iteration
-        vector<Matrix<float,Dynamic,Dynamic>> hidden_deltas;
-        Matrix<float,Dynamic,10> output_delta;
+        vector<Matrix<float,60,60>> hidden_deltas;
+        Matrix<float,60,10> output_delta;
         
         //row-matrix of current neurons values
-        vector<Matrix<float,1,Dynamic>> hidden_layers_values;
+        vector<Matrix<float,1,60>> hidden_layers_values;
         Matrix<float,1,10> output_layer_values;
         
         //matrix of next possible neurons values, according to the chosen action
-        vector<Matrix<float,1,Dynamic>> test_hidden_layers_values;
+        vector<Matrix<float,1,60>> test_hidden_layers_values;
         Matrix<float,1,10> test_output_layer_values;
         
         //the number of neurons layer
@@ -44,16 +44,16 @@ class Ai {
         ~Ai();
         void save_ai();
         void initialise_random_ai();
-        void load_trained_ai(int difficulty);
-        void forward_propagation(Matrix<float,1,20> game_state);
-        void test_forward_propagation(Matrix<float,1,20> game_state);
+        void load_trained_ai();
+        void forward_propagation(Matrix<float,1,60> game_state);
+        void test_forward_propagation(Matrix<float,1,60> game_state);
         int choose_action(Matrix<float,1,10> actions);
-        int play(Matrix<float,1,20> game_state);
+        int play(Matrix<float,1,60> game_state);
         void backward_propagation(float q_targets[10]);
-        Matrix<float,1,20> play_simulation(Matrix<float,1,20> game_state, int action);
-        int test_win(Matrix<float,1,20> state);
+        Matrix<float,1,60> play_simulation(Matrix<float,1,60> game_state, int action);
+        int test_win(Matrix<float,1,60> state);
         float max_output_test();
-        int play_learn(Matrix<float,1,20> game_state);
+        int play_learn(Matrix<float,1,60> game_state);
 };
 
 float randomFloat(float a, float b);
