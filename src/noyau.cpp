c46eb014a9f30bd2e8fc50d7b10831d93381da44
VictorCostantino <victor.du.67@hotmail.fr>
2018-04-06 11:04:35 +0200

    switch perfect

--- src/noyau.cpp
+++ src/noyau.cpp
@@ -187,7 +187,9 @@ void Noyau::poser_test(int x)
     else if(!isAdv(x) && isMonst(x) && trouver(x)!=NULL )
     {
         switch_position(x);
-        emit tiens("swap/"+x);
+        std::stringstream s1;
+        s1 << "swap/" << Carte::correspondant(x);
+        emit tiens(QString::fromStdString(s1.str()));
     }
 }
 
