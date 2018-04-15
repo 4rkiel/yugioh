f65522c27a531d7c81e2d767b7f0dbeeaea92d50
Skytrock <haller.mathieu67@gmail.com>
2018-04-15 12:53:59 +0200

    clavier

--- src/main.cpp
+++ src/main.cpp
@@ -393,7 +393,8 @@ int main(int argc, char *argv[]) {
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 
     // Load main widget
-
+    Keyfilter k;
+    app.installEventFilter(&k);
 
     Window w(&app,&qtTranslator,&YugiTranslator);
 
